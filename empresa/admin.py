d84152e0e768cb3065949b8f1b75bddb3e306f76
João Eduardo Ferreira <joao.eduardoferco@gmail.com>
2022-12-16 16:59:59 -0300

    Primeiro update do projeto
    primeiro update público do projeto ainda sem reestruturação do banco de dados

---
+++ empresa/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Empresa, Tecnologias, Vagas
+
+admin.site.register(Tecnologias)
+admin.site.register(Empresa)
+admin.site.register(Vagas)
+
+# Register your models here.
